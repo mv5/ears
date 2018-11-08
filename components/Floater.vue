eba579cda24782debc467b90aba2505daab49a9b
menashe <menash88@gmail.com>
2018-11-08 08:48:01 +0200

    added contact floater

---
+++ components/Floater.vue
@@ -0,0 +1,27 @@
+<template>
+      <v-btn
+              color="blue"
+              dark
+              large
+              fixed
+              bottom
+              fab
+              @click="openDialog"
+            >
+              <v-icon>phone_in_talk</v-icon>
+            </v-btn>
+</template>
+
+<script>
+export default {
+  methods: {
+    openDialog(){
+      this.$store.commit('toggleDialog', true)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
