1133e8f4dc89676bb9ddae95aa0575029b7126f4
menashe <menash88@gmail.com>
2018-11-08 08:48:39 +0200

    added about page

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-layout>
+    <v-flex text-xs-center>
+     
+       <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <p>to be continued</p>
+                    </v-card-text>
+                  
+                  </v-card>
+    </v-flex>
+  </v-layout>
+</template>
