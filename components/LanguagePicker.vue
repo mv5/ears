f73562c276b696aa1698bfb1c8ce1365cf24d1de
menashe <menash88@gmail.com>
2018-11-08 08:48:26 +0200

    added language picker

---
+++ components/LanguagePicker.vue
@@ -0,0 +1,47 @@
+<template>
+      <v-select
+          :items="items"
+          item-text="name"
+          item-value="value"
+          :value="currLang"
+          :label="label"
+          @change="changeLang"
+        ></v-select>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+    data(){
+        return {
+            items: [
+                {
+                    name: "English",
+                    value: "en"
+                },
+                {
+                    name: "עברית",
+                    value: "he"
+                }
+            ],
+        }
+    },
+    methods: {
+        changeLang(lang){
+            this.$store.commit('changeLang', lang)
+        }
+    },
+    computed: mapState([
+        'currLang',
+        'label'
+    ])
+}
+</script>
+
+<style scoped>
+    .v-select{
+        width: 5vw;
+        flex: 0 0 auto;
+    }
+</style>
