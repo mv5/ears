12c8ad4a7acff3d09a9f668df60cc60d9503a126
menashe <menash88@gmail.com>
2018-11-13 12:24:29 +0200

    extracted header and footer

---
+++ components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+      <v-footer class="blue darken-2">
+        <v-layout row wrap align-center>
+          <v-flex xs12>
+            <div class="white--text text-xs-center">
+        
+              <a class="white--text" href="#" target="_blank">{{ $t('company') }}</a>
+            </div>
+          </v-flex>
+        </v-layout>
+      </v-footer>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: mapState(['currLang']),
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+        <v-toolbar class="white" fixed app>
+          <nuxt-link class="link" to="/">
+            <v-toolbar-title class="title" v-text="$t('logo')"></v-toolbar-title>
+          </nuxt-link>
+
+          <nuxt-link class="link" to="/about">{{ $t('links.about')}}</nuxt-link>
+
+      <LanguagePicker />
+    </v-toolbar>
+
+</template>
+
+<script>
+import LanguagePicker from '~/components/LanguagePicker.vue'
+import { mapState } from 'vuex'
+
+export default {
+  components: {
+    LanguagePicker
+  },
+  computed: mapState(['currLang']),
+}
+</script>
+
+<style scoped>
+.link {
+  margin: 0 20px;
+}
+</style>
