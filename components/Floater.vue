3141f1f78deec8aabdf2b674a641665ac8b1db3f
menashe <menash88@gmail.com>
2018-11-21 14:54:55 +0200

    modified floater

--- components/Floater.vue
+++ components/Floater.vue
@@ -1,7 +1,34 @@
 <template>
-  <v-btn class="floater" color="blue" dark large fixed fab @click="openDialog">
-    <v-icon>phone_in_talk</v-icon>
-  </v-btn>
+  <div class="floater">
+    <v-tooltip top>
+      <v-btn 
+        dark 
+        :large="$vuetify.breakpoint.smAndDown ? false : true"
+        :medium="$vuetify.breakpoint.smAndDown ? true : false"
+        fab
+        class="primary lighten-1" 
+        href="tel:1234567"
+        slot="activator"
+      >
+        <v-icon>phone_in_talk</v-icon>
+      </v-btn>
+      <span>{{ $t('contactDetails.phone') }}</span>
+    </v-tooltip>
+    <v-tooltip top>
+      <v-btn 
+        dark 
+        :large="$vuetify.breakpoint.smAndDown ? false : true"
+        :medium="$vuetify.breakpoint.smAndDown ? true : false"
+        fab
+        @click="openDialog"
+        class="primary lighten-1" 
+        slot="activator"
+      >
+        <v-icon>email</v-icon>
+      </v-btn>
+      <span>{{ $t('contactDetails.email') }}</span>
+    </v-tooltip>
+  </div>
 </template>
 
 <script>
@@ -16,6 +43,9 @@ export default {
 
 <style scoped>
 .floater {
-  top: 75vh;
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  top: 50vh;
 }
 </style>
